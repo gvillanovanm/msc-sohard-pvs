c77c79180b528141a25da1c2726b9b78995ef34c
Gabriel Villanova <gabriel.magalhaes@virtus.ufcg.edu.br>
2023-07-03 21:09:42 -0400

    chore: updates code ips_CASE1* and ips_CASE2* to be equal to ips_CASE0*, except the parameters related to heart conditions

--- project/ips_CASE1_dis_SA.m
+++ project/ips_CASE1_dis_SA.m
@@ -17,116 +17,116 @@ clc;
 % ----------------------------------------------------------------------- %
 % ON/OFF
 % ----------------------------------------------------------------------- %
-PM_ON                   = 1;                                % 0:OFF/1:ON
-fixed_step              = .00001;
+PM_ON                     = 1;                              % 0:OFF/1:ON
+fixed_step                = .00001;
 
 % ----------------------------------------------------------------------- %
 % BATTERY
 % ----------------------------------------------------------------------- %
-E_battery               = 2.8;                              % [V]   
-nonc                    = 2.8-2.679;                        % [V]
-Rohm                    = 2.800;                            % [Ohms]
-w1                      = 10^(4);                           % [Hz]
-Cg                      = 1/(Rohm*w1);                      % [F]
-Rnohm                   = 4.5;                              % [Ohms]
-w2                      = 3.9;                              % [Hz]
-Rct                     = ((2.679-2.467)/.050)-Rohm;        % [Ohms]
-Cdl                     = 1/(Rnohm*w2);                     % [F]
-
-% old battery model:
-% R_battery_internal      = 100;                            % [Ohms]
-% ampHourRating           = 1;                              % [hr*A]
-% AH1                     = 2;                              % [V]
-% AHI_noload              = .01;                            % [hr*A]
+E_battery                 = PM_ON*2.8;                      % [V]   
+nonc                      = 2.8-2.679;                      % [V]
+Rohm                      = 2.800;                          % [Ohms]
+w1                        = 10^(4);                         % [Hz]
+Cg                        = 1/(Rohm*w1);                    % [F]
+Rnohm                     = 4.5;                            % [Ohms]
+w2                        = 3.9;                            % [Hz]
+Rct                       = ((2.679-2.467)/.050)-Rohm;      % [Ohms]
+Cdl                       = 1/(Rnohm*w2);                   % [F]
 
 % ----------------------------------------------------------------------- %
 % TELEMETRY
 % ----------------------------------------------------------------------- %
-i_telemetry_curDrain    = 60e-6;                            % [A]
-R_telemetry             = E_battery/i_telemetry_curDrain;   % [Ohms]
+i_telemetry_curDrain      = 1e-6;                            % [A]
+R_telemetry               = E_battery/i_telemetry_curDrain; % [Ohms]
 
 % ----------------------------------------------------------------------- %
 % ATRIAL PACING
 % ----------------------------------------------------------------------- %
+enable_fail_to_capture    = 0;
+
 if(fixed_step==.0001)
-    aPulseWidth         = 4;                                % [10-4s] 
-    aDoubleVoltEn       = 1;                                % 0=2.5V/1=5V 
+    aPulseWidth           = 4;                              % [10-4s] 
 else
-    % capture:
-    aPulseWidth         =  1*100;                           % [10-5s] 
-    % failure-to-capture
-    %aPulseWidth         = .1*100;                           % [10-5s] 
-    aDoubleVoltEn       = 1;                                % 0=2.5V/1=5V 
+    aPulseWidth           = (.2)*100;                       % [10-5s] 
+
+    if(enable_fail_to_capture)
+        aPulseWidth       = .1*100;                         % [10-5s] 
+    end
 end
-T_aSwitch               = .001;                             % [s]
-Cp_a                    = 10;                               % [uF]
-Co_a                    = 10;                               % [uF]
+
+aDoubleVoltEn             = 1;                              % 0=2.5V/1=5V 
+T_aSwitch                 = .001;                           % [s]
+Cp_a                      = 10;                             % [uF]
+Co_a                      = 10;                             % [uF]
+internal_resistance       = .01;
 
 % ----------------------------------------------------------------------- %
-% ATRIAL SENSING (LPF fc = 100Hz)
+% ATRIAL SENSING  (LPF fc = 100Hz)
+% ATRIAL SENSING  (LPF fc = 300Hz)
 % ----------------------------------------------------------------------- %
-aSensitivity            = 5;                                % [ < aS < ][mV]
+aSensitivity              = 2;                              % [ < aS < ][mV]
 
-R_aFilterSensing        = 7.95e+3;                          % [Ohms]
-C_aFilterSensing        = 1e-6;                             % [F]
+R_aFilterSensing          = 100;                            % [Ohms]
+C_aFilterSensing          = 16e-6;                          % [F]
 
-R1_aSensingAmplifier    = 1;                                % [Ohms]
-R2_aSensingAmplifier    = 10;                               % [Ohms]
+R1_aSensingAmplifier      = 1;                              % [Ohms]
+R2_aSensingAmplifier      = 10;                             % [Ohms]
 
-R_aPowerAmplifierAmp    = 1000000;                          % [Ohms]
-R_aPowerComparatorAmp   = 1000000;                          % [Ohms]
+R_aPowerAmplifierAmp      = 2800000;                        % [Ohms]
+R_aPowerComparatorAmp     = 2800000;                        % [Ohms]
 
 % ----------------------------------------------------------------------- %
 % VENTRICLE PACING
 % ----------------------------------------------------------------------- %
 if(fixed_step==.0001)
-    vPulseWidth          = 4;                                % [10-4s] 
-    T_vSwitch            = .001;                             % [s]
+    vPulseWidth           = 4;                              % [10-4s] 
 else
-    vPulseWidth          = 1*100;                            % [10-5s] 
-    T_vSwitch            = .001;                             % [s]
+    vPulseWidth           = (.2)*100;                       % [() ms] 
 end
-vDoubleVoltEn           = 1;                                % 0=2.5V/1=5V 
-Cp_v                    = 10;                               % [uF]
-Co_v                    = 10;                               % [uF]
+
+vDoubleVoltEn             = 1;                              % 0=2.5V/1=5V 
+T_vSwitch                 = .001;                           % [s]
+Cp_v                      = 10;                             % [uF]
+Co_v                      = 10;                             % [uF]
 
 % ----------------------------------------------------------------------- %
 % VENTRICLE SENSING 
 % ----------------------------------------------------------------------- %
-vSensitivity            = 5;                                % [ < vS < ][mV]
+vSensitivity              = 2;                              % [ < vS < ][mV]
 
-R_vFilterSensing        = 7.95e+3;                          % [Ohms]
-C_vFilterSensing        = 1e-6;                             % [F]
+R_vFilterSensing          = 100;                            % [Ohms]
+C_vFilterSensing          = 16e-6;                          % [F]
 
-R1_vSensingAmplifier    = 1;                                % [Ohms]
-R2_vSensingAmplifier    = 10;                               % [Ohms]
+R1_vSensingAmplifier      = 1;                              % [Ohms]
+R2_vSensingAmplifier      = 10;                             % [Ohms]
 
-R_vPowerAmplifierAmp    = 1000000;                          % [Ohms]
-R_vPowerComparatorAmp   = 1000000;                          % [Ohms]
+R_vPowerAmplifierAmp      = 2800000;                        % [Ohms]
+R_vPowerComparatorAmp     = 2800000;                        % [Ohms]
 
 % ----------------------------------------------------------------------- %
 % AUTOMATA MODEL 
 % ----------------------------------------------------------------------- %
 if(fixed_step==.0001)
-    LRIdef                  = 7500   -1;                        % [10-4s]
-    AVIdef                  = 2300   -1;                        % [10-4s] 
-    ARPdef                  = 1500   -1;                        % [10-4s]
-    VRPdef                  = 2000   -1;                        % [10-4s]
-    URIdef                  = 8000   -1;                        % [10-4s] 
-    BPM_programmed          = 1/((LRIdef)/10000)*60;            % [BPM]
+    LRIdef                = 10000  -1;                      % [10-4s]
+    AVIdef                = 1700   -1;                      % [10-4s] 
+    ARPdef                = 2500   -1;                      % [10-4s]
+    VRPdef                = 2300   -1;                      % [10-4s]
+    URIdef                = 5000   -1;                      % [10-4s] 
+    BPM_programmed        = 1/((LRIdef)/10000)*60;          % [BPM]
 else
-    LRIdef                  = 75000   -1;                        % [10-5s]
-    AVIdef                  = 23000   -1;                        % [10-5s] 
-    ARPdef                  = 15000   -1;                        % [10-5s]
-    VRPdef                  = 20000   -1;                        % [10-5s]
+    TLRI                  = 100000  -1;                     % [10-5s]
+    TAVI                  = 17000   -1;                     % [10-5s] 
+    TARP                  = 25000   -1;                     % [10-5s]
+    TVRP                  = 23000   -1;                     % [10-5s]
+
     % https://www.seas.upenn.edu/~lee/09cis480/lec-pm.pdf
     % URI must be longer than TARP (AVI + PVARP), otherwise it has no effect. 
-    URIdef                  = AVIdef+VRPdef+1;                  % [10-4s] 
-    BPM_programmed          = 1/((LRIdef)/100000)*60;            % [BPM]
+    
+    TURI                  = 50000  -1;                      % [10-4s] 
+    BPM_programmed        = 1/((TLRI)/100000)*60;           % [BPM]
 end
 
 %%
-
 % ======================================================================= %
 %
 % HEART MODEL
@@ -136,136 +136,105 @@ end
 % ----------------------------------------------------------------------- %
 % BPM heart
 % ----------------------------------------------------------------------- %
-BPM                     = 40;                               % [BPM]
+BPM                       = 40;                             % [BPM]
 
 % ----------------------------------------------------------------------- %
 % Atrial and Ventricular Zlead (source: [1975][BOOK]Advances in PM Tech..)
 % ----------------------------------------------------------------------- %
-R_atrialLead            = 50;                               % [Ohms]
-Rf_atrialLead           = 80;                               % [Ohms]
-CH_atrialLead           = 3;                                % [uF]
+R_atrialLead              = 50;                             % [Ohms]
+Rf_atrialLead             = 80;                             % [Ohms]
+CH_atrialLead             = 3;                              % [uF]
 
-R_ventricularLead       = 50;                               % [Ohms]
-Rf_ventricularlLead     = 80;                               % [Ohms]
-CH_ventricularLead      = 3;                                % [uF]
+R_ventricularLead         = 50;                             % [Ohms]
+Rf_ventricularlLead       = 80;                             % [Ohms]
+CH_ventricularLead        = 3;                              % [uF]
 
 % ----------------------------------------------------------------------- %
 % Atrial and Ventricular Ztissue (source: [1975][BOOK]Advances in PM Tech.)
 % ----------------------------------------------------------------------- %
-Rt_atrial               = 500;                              % [Ohms]
-at_Vrheo                = 1.4;
-tc_atrial               = .3*.001;
+Rt_atrial                 = 500;                            % [Ohms]
+at_Vrheo                  = 1.4;
+tc_atrial                 = .3*.001;
 
-Rt_ventricle            = 500;                              % [Ohms]
-ve_Vrheo                = 1.4;
-tc_ventricular          = .3*.001;
+Rt_ventricle              = 500;                            % [Ohms]
+ve_Vrheo                  = 1.4;
+tc_ventricular            = .3*.001;
 
 % ----------------------------------------------------------------------- %
 % EGM select and noise (source: Inrich A=10mV)
 % ----------------------------------------------------------------------- %
-AEGM_sel                = 0;                                % 0:ISO/1:Dip;
-VEGM_sel                = 0;                                % 0:ISO/1:Dip;
-noisePower_at           = 2e-10;
-noisePower_ven          = 2e-10;                             
+AEGM_sel                  = 0;                              % 0:ISO/1:Dip;
+VEGM_sel                  = 1;                              % 0:ISO/1:Dip;
+enable_oversensing        = 0;
 
+if(enable_oversensing)
+    noisePower_at         = 4e-11;
+    noisePower_ven        = 2e-11;                             
+else
+    noisePower_at         = 2e-12;
+    noisePower_ven        = 5e-11;
+end
+
+%%
 % ----------------------------------------------------------------------- %
 % SA node (NA1_SA) (def BPM)
 % ----------------------------------------------------------------------- %
-Terp_sta_SA             = 3200;                             % [10-4s] 
-Terp_def_SA             = 3200;                             % [10-4s]  
-Terp_min_SA             = 1500;                             % [10-4s]  
-Terp_max_SA             = 3000;                             % [10-4s] 
-Trrp_sta_SA             = 1200;                             % [10-4s]  
-Trrp_def_SA             = 1200;                             % [10-4s]  
-Tres_sta_SA             = (1/BPM)*60*10000-(Terp_min_SA+Trrp_sta_SA); % [BPM]
-Tres_def_SA             = Tres_sta_SA;                      % [10-4s]
+Terp_sta_SA               = 3200;                           % [10-4s] 
+Terp_def_SA               = 3200;                           % [10-4s]  
+Terp_min_SA               = 1500;                           % [10-4s]  
+Terp_max_SA               = 3000;                           % [10-4s] 
+Trrp_sta_SA               = 1200;                           % [10-4s]  
+Trrp_def_SA               = 1200;                           % [10-4s]  
+Tres_sta_SA               = (1/BPM)*60*10000-(Terp_min_SA+Trrp_sta_SA); % [BPM]
+Tres_def_SA               = Tres_sta_SA;                    % [10-4s]
 
+% ----------------------------------------------------------------------- %
+% AVI node (NA2_AV)
+% ----------------------------------------------------------------------- %
+Terp_sta_AV               = 3200;                           % [10-4s] 
+Terp_def_AV               = 3200;                           % [10-4s] 
+Trrp_sta_AV               = 1200;                           % [10-4s] 
+Trrp_def_AV               = 1200;                           % [10-4s] 
+Tres_sta_AV               = 99999;                          % [10-4s] 
+Tres_def_AV               = 99999;                          % [10-4s] 
+Terp_min_AV               = 1500;                           % [10-4s] 
+Terp_max_AV               = 3000;                           % [10-4s] 
 
 % ----------------------------------------------------------------------- %
 % AV path node (PA1_1to3) (the default is 220 for all)
 % ----------------------------------------------------------------------- %
-Kpath1to3               = 3;
-T_min_path_1to3         = 220*Kpath1to3;                    % [10-4s]   
-T_bac_path_1to3         = 220*Kpath1to3;                    % [10-4s]  
-T_fwd_path_1to3         = 220*Kpath1to3;                    % [10-4s]  
+Kpath1to3                 = 1;
+T_min_path_1to3           = 220*Kpath1to3;                  % [10-4s]   
+T_bac_path_1to3           = 220*Kpath1to3;                  % [10-4s]  
+T_fwd_path_1to3           = 220*Kpath1to3;                  % [10-4s]  
 
 % ----------------------------------------------------------------------- %
 % AV path node (PA1_1to7) (the default is 600 for all)
 % ----------------------------------------------------------------------- %
-Kpath1to7               = 3;
-T_min_path_1to7         = 600*Kpath1to7;
-T_back_path_1to7        = 600*Kpath1to7;
-T_fwd_path_1to7         = 600*Kpath1to7;
+Kpath1to7                 = 1;
+T_min_path_1to7           = 600*Kpath1to7;
+T_back_path_1to7          = 600*Kpath1to7;
+T_fwd_path_1to7           = 600*Kpath1to7;
 
-
-
-% don't touch here
 % ----------------------------------------------------------------------- %
-% AVI node (NA2_AV)
+% RVA node
 % ----------------------------------------------------------------------- %
-Terp_sta_AV             = 3200;                             % [10-4s] 
-Terp_def_AV             = 3200;                             % [10-4s] 
-Trrp_sta_AV             = 1200;                             % [10-4s] 
-Trrp_def_AV             = 1200;                             % [10-4s] 
-Tres_sta_AV             = 99999;                            % [10-4s] 
-Tres_def_AV             = 99999;                            % [10-4s] 
-Terp_min_AV             = 1500;                             % [10-4s] 
-Terp_max_AV             = 3000;                             % [10-4s] 
+Krva                      = 1; 
+Terp_sta_RVA              = 3200*Krva;                      % [10-4s] 
+Terp_def_RVA              = 3200*Krva;                      % [10-4s] 
+Trrp_sta_RVA              = 1200;                           % [10-4s] 
+Trrp_def_RVA              = 1200;                           % [10-4s] 
+Terp_min_RVA              = 1500;                           % [10-4s] 
+Terp_max_RVA              = 3000*Krva;                      % [10-4s] 
 
 % ----------------------------------------------------------------------- %
 % Bach node (AV)
 % ----------------------------------------------------------------------- %
-Terp_sta_BA             = 3200;                             % [10-4s] 
-Terp_def_BA             = 3200;                             % [10-4s]  
-Terp_min_BA             = 1500;                             % [10-4s]  
-Terp_max_BA             = 3000;                             % [10-4s] 
-Trrp_sta_BA             = 1200;                             % [10-4s]  
-Trrp_def_BA             = 1200;                             % [10-4s]  
-Tres_sta_BA             = 99999;
-Tres_def_BA             = 99999;                            % [10-4s]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%%
-
-% ======================================================================= %
-%
-% NOTES
-%
-% ======================================================================= %
-
-% AUTOMATA PACEMAKER MODEL:
-% how to chose these paremeters to define a specific BPM?
-% With a programmed sensed and paced AV interval (AVI) of 160 and 190 ms, respectively.
-% https://thoracickey.com/6-pacemaker-timing-cycles-and-special-features/
-
-% INPUT
-    % LRIdef    = 900; 
-    % AVIdef    = 300; 
-    % BPM_heart = 50, 60;
-    
-% OUTPUT
-    % BPM_pm    = 1/.9*60 = 67 BPM;
+Terp_sta_BA               = 3200;                           % [10-4s] 
+Terp_def_BA               = 3200;                           % [10-4s]  
+Terp_min_BA               = 1500;                           % [10-4s]  
+Terp_max_BA               = 3000;                           % [10-4s] 
+Trrp_sta_BA               = 1200;                           % [10-4s]  
+Trrp_def_BA               = 1200;                           % [10-4s]  
+Tres_sta_BA               = 99999;                          % [10-4s]
+Tres_def_BA               = 99999;                          % [10-4s]
